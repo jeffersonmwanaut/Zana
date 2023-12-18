43edbeeec07da3edfc32a1b735b8a13c3ec32c8b
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2023-12-18 15:37:54 +0200

    Update dev.php
    Put placeholder values

--- config/dev.php
+++ config/dev.php
@@ -2,10 +2,10 @@
 return [
     'db' => [
         'mysql' => [
-            'host' => 'localhost',
-            'name' => 'dbname',
-            'user' => 'root',
-            'password' => '',
+            'host' => 'YOUR_HOST',
+            'name' => 'YOUR_DATABASE_NAME',
+            'user' => 'YOUR_DATABASE_USERNAME',
+            'password' => 'YOUR_DATABASE_USER_PASSWORD',
             'charset' => 'utf8',
             'collation' => 'utf8mb4_general_ci',
             'prefix' => ''

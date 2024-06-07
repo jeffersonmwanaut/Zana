7c75f22dc591d17aac41178aed1998ed86b72c72
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-06-07 13:44:37 -0700

    Fix PHP8 deprecation bugs

--- vendor/zana/Entity/JsonSerializableEntity.php
+++ vendor/zana/Entity/JsonSerializableEntity.php
@@ -15,7 +15,7 @@ class JsonSerializableEntity extends Entity implements \JsonSerializable
      * which is a value of any type other than a resource.
      * @since 5.4.0
      */
-    public function jsonSerialize()
+    public function jsonSerialize():mixed
     {
         return get_object_vars($this);
     }

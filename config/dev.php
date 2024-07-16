1bdd71bd92cd018863cbf613f6f02e216a235450
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-07-16 15:38:09 +0200

    Update dev.php
    Add documentation to config/dev.php

--- config/dev.php
+++ config/dev.php
@@ -1,4 +1,16 @@
 <?php
+/** 
+This PHP array configuration sets up a MySQL database connection.
+You'll need to replace the placeholders with your actual database details:
+
+- 'host' with your database host (e.g., `localhost`).
+- 'name' with your database name.
+- 'user' with your database username.
+- 'password' with your database password.
+
+The 'charset' and 'collation' settings ensure the database uses UTF-8 encoding, which supports a wide range of characters.
+The 'prefix' can be used if you want to add a prefix to all your database tables. 
+*/
 return [
     'db' => [
         'mysql' => [

cc63ed7a5d8267236a184566aa220dae175c07a1
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-07-20 19:44:49 -0700

    Update base.template.php
    Make alert dismissible

--- vendor/zana/template/base.template.php
+++ vendor/zana/template/base.template.php
@@ -3,15 +3,15 @@ require 'partial/base-header.php';
 ?>
 
 <?php if ($session::hasFlash('success')): ?>
-    <div class="alert alert-success mt-2">
-    <i class="fa-solid fa-circle-check me-2"></i>
-        <?= $session::getFlash('success') ?>
+    <div class="alert alert-success alert-dismissible mt-2" role="alert">
+        <div><i class="fa-solid fa-circle-check me-2"></i><?= $session::getFlash('success') ?></div>
+        <a class="btn-close" href="<?= $httpRequest->requestUri() ?>"></a>
     </div>
 <?php endif; ?>
 <?php if ($session::hasFlash('danger')): ?>
-    <div class="alert alert-danger mt-2">
-    <i class="fas fa-exclamation-triangle me-2"></i>
-        <?= $session::getFlash('danger') ?>
+    <div class="alert alert-danger alert-dismissible mt-2" role="alert">
+        <div><i class="fas fa-exclamation-triangle me-2"></i><?= $session::getFlash('danger') ?></div>
+        <a class="btn-close" href="<?= $httpRequest->requestUri() ?>"></a>
     </div>
 <?php endif; ?>
 

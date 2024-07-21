c6fe064b059bf9726afd2473257a923eec38b9f8
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-07-21 10:19:05 -0700

    Add Form Validator

---
+++ vendor/zana/Form/FormValidator.php
@@ -0,0 +1,79 @@
+<?php namespace Zana\Form;
+
+class FormValidator {
+    private $errors = [];
+    private $data = [];
+  
+    public function __construct($data) {
+        $this->data = $data;
+    }
+  
+    public function validate($rules) {
+        foreach ($rules as $field => $rule) {
+            if(!isset($this->data[$field])) continue;
+            $value = $this->data[$field];
+            $ruleParts = explode('|', $rule);
+    
+            foreach ($ruleParts as $rulePart) {
+                if ($this->validateRule($field, $value, $rulePart) === false) {
+                    $this->addError($field, $rulePart);
+                }
+            }
+        }
+    
+        return !$this->hasErrors();
+    }
+  
+    private function validateRule($field, $value, $rule) {
+        $ruleKey = explode(':', $rule)[0];
+        switch ($ruleKey) {
+            case 'required':
+                return !empty($value);
+            case 'email':
+                if(filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                    $emailParts = explode('@', $value);
+                    $domain = $emailParts[1];
+                    return checkdnsrr($domain . '.', 'MX');
+                }
+                return false;
+            case 'minLength':
+                $length = (int) explode(':', $rule)[1];
+                return strlen($value) >= $length;
+            case 'maxLength':
+                $length = (int) explode(':', $rule)[1];
+                return strlen($value) <= $length;
+            default:
+                return true;
+        }
+    }
+  
+    private function addError($field, $rule) {
+        $this->errors[$field][] = $this->getErrorMessage($rule);
+    }
+  
+    private function getErrorMessage($rule) {
+        $ruleKey = explode(':', $rule)[0];
+        switch ($ruleKey) {
+            case 'required':
+                return 'This field is required.';
+            case 'email':
+                return 'Invalid email address.';
+            case 'minLength':
+                $length = (int) explode(':', $rule)[1];
+                return "Minimum length is $length characters.";
+            case 'maxLength':
+                $length = (int) explode(':', $rule)[1];
+                return "Maximum length is $length characters.";
+            default:
+                return 'Invalid input.';
+        }
+    }
+  
+    public function hasErrors() {
+        return !empty($this->errors);
+    }
+  
+    public function getErrors() {
+        return $this->errors;
+    }
+}

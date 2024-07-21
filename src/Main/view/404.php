5730337f5616e1e8f63fd7c89439f97df39d8c97
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-07-21 11:45:23 -0700

    Update 404.php

--- src/Main/view/404.php
+++ src/Main/view/404.php
@@ -1,6 +1,6 @@
 <div class="row justify-content-center mt-5">
     <div class="col-md-5">
-        <h1>404 Error: Page Not Found</h1>
+        <h1 style="color: #AC3F3D; font-weight: lighter">404 Error: Page Not Found</h1>
         <p>The page you're looking for doesn't exist or has been removed.</p>
         <p><a href="<?= $router::generateUrl('_MAIN') ?>">Return to Homepage</a></p>
     </div>

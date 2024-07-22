4482adf43c6b85737e53a304c4108f4aa711bd26
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-07-22 15:08:55 +0200

    Update UrlBuilder.php: Supports generating URLs with query parameters, anchor tags, and URLs that require authentication or authorization

--- vendor/zana/Router/UrlBuilder.php
+++ vendor/zana/Router/UrlBuilder.php
@@ -4,6 +4,9 @@ class UrlBuilder {
     private $route;
     private $routeParameters = [];
     private $queryStringParameters = [];
+    private $anchor;
+    private $requiresAuthentication = false;
+    private $requiresAuthorization = false;
 
     public function setRoute($route) {
         $this->route = $route;
@@ -20,10 +23,38 @@ class UrlBuilder {
         return $this;
     }
 
+    public function setAnchor($anchor) {
+        $this->anchor = $anchor;
+        return $this;
+    }
+
+    public function requiresAuthentication($value = true) {
+        $this->requiresAuthentication = $value;
+        return $this;
+    }
+
+    public function requiresAuthorization($value = true) {
+        $this->requiresAuthorization = $value;
+        return $this;
+    }
+
     public function build() {
         $url = Router::generateUrl($this->route, $this->routeParameters);
         if (!empty($this->queryStringParameters)) {
-            $url.= '?'. http_build_query($this->queryStringParameters);
+            $url .= '?' . http_build_query($this->queryStringParameters);
+        }
+        if (!empty($this->anchor)) {
+            $url .= '#' . $this->anchor;
+        }
+        if ($this->requiresAuthentication) {
+            // Add authentication token or login URL to the URL
+            // For example:
+            $url .= '?auth_token=' . Auth::getToken();
+        }
+        if ($this->requiresAuthorization) {
+            // Add authorization token or permission check to the URL
+            // For example:
+            $url .= '?permission=' . Permission::CHECK_ADMIN;
         }
         return $url;
     }

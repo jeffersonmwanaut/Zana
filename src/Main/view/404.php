d865e4a646e2ef85d2acbc1aa72c83d1e7ff677b
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-07-22 13:55:30 +0200

    Update 404.php

--- src/Main/view/404.php
+++ src/Main/view/404.php
@@ -1,7 +1,8 @@
 <div class="row justify-content-center mt-5">
     <div class="col-md-5">
         <h1 style="color: #AC3F3D; font-weight: lighter">404 Error: Page Not Found</h1>
-        <p>The page you're looking for doesn't exist or has been removed.</p>
-        <p><a href="<?= $router::generateUrl('_MAIN') ?>">Return to Homepage</a></p>
+        <h2>Oops, it looks like the page you're looking for doesn't exist!</h2>
+        <p>The page you're looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+        <p>Try checking the URL for errors, or <a href="<?= $router::generateUrl('_MAIN') ?>">return to the homepage</a> to start fresh.</p>
     </div>
 </div>

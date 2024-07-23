92c13eceadc12a67d240f836e95e652ed0a8d699
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-07-23 16:17:46 +0200

    Update UrlBuilder.php: Remove the requiresAuthentication() and requiresAuthorization() methods which can be a security risk.

--- vendor/zana/Router/UrlBuilder.php
+++ vendor/zana/Router/UrlBuilder.php
@@ -5,8 +5,6 @@ class UrlBuilder {
     private $routeParameters = [];
     private $queryStringParameters = [];
     private $anchor;
-    private $requiresAuthentication = false;
-    private $requiresAuthorization = false;
 
     public function setRoute($route) {
         $this->route = $route;
@@ -28,16 +26,6 @@ class UrlBuilder {
         return $this;
     }
 
-    public function requiresAuthentication($value = true) {
-        $this->requiresAuthentication = $value;
-        return $this;
-    }
-
-    public function requiresAuthorization($value = true) {
-        $this->requiresAuthorization = $value;
-        return $this;
-    }
-
     public function build() {
         $url = Router::generateUrl($this->route, $this->routeParameters);
         if (!empty($this->queryStringParameters)) {
@@ -45,16 +33,6 @@ class UrlBuilder {
         }
         if (!empty($this->anchor)) {
             $url .= '#' . $this->anchor;
-        }
-        if ($this->requiresAuthentication) {
-            // Add authentication token or login URL to the URL
-            // For example:
-            // $url .= '?auth_token=' . Auth::getToken();
-        }
-        if ($this->requiresAuthorization) {
-            // Add authorization token or permission check to the URL
-            // For example:
-            // $url .= '?permission=' . Permission::CHECK_ADMIN;
         }
         return $url;
     }

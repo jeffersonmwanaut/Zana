59e35c68526ab1e48ba6f01c2ebc3706fc99b58a
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-09-19 19:38:01 -0700

    Make app.js a module

--- vendor/zana/template/partial/base-footer.php
+++ vendor/zana/template/partial/base-footer.php
@@ -4,6 +4,6 @@
     <script type="text/javascript" src="<?= BOOTSTRAP['js']['core'] ?>"></script>
     <!-- Bootstrap JS Tooltips -->
     <script type="text/javascript" src="<?= BOOTSTRAP['js']['popper'] ?>"></script>
-    <script src="<?= JS ?>/app.js"></script>
+    <script type="module" src="<?= JS ?>/app.js"></script>
 </body>
 </html>

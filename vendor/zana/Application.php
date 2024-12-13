1b25e14819f456bd118d7870c9d2107cdf213532
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-12-13 14:19:28 +0200

    Improve Application class PHPDoc

--- vendor/zana/Application.php
+++ vendor/zana/Application.php
@@ -6,33 +6,40 @@ use Zana\Config\Config;
 use Zana\Router\Router;
 use Zana\Router\Route;
 use Zana\Session\Session;
+use Exception;
 
 /**
- * Create by: Jefferson Mwanaut
- * Last modified: 2012-12-11 00:58
+ * Class Application
+ * Manages the application lifecycle, including session management, routing, and module loading.
  */
 class Application extends Singleton
 {
     /**
      * Modules of the application.
-     * @var string[]
+     * @var array
      */
-    protected static $modules = [];
+    protected static array $modules = [];
 
     public function __construct()
     {
         Session::start();
         Router::getInstance();
 
-        foreach (Config::get('modules') as $module)
-        {
+        foreach (Config::get('modules') as $module) {
             $module = preg_replace('#/#', '\\', $module);
-            if($module[0] !== '\\') $module = '\\' . $module;
+            if ($module[0] !== '\\') {
+                $module = '\\' . $module;
+            }
             self::$modules[] = new $module();
         }    
     }
 
-    public static function run()
+    /**
+     * Runs the application, handling routing and HTTP responses.
+     *
+     * @return void
+     */
+    public static function run(): void
     {
         $httpResponse = new HttpResponse();
         try {
@@ -40,10 +47,10 @@ class Application extends Singleton
             $httpResponse->setPage($page);
             $httpResponse->send();
         } catch (Exception $e) {
-            if(Config::mode() !== 'prod') {
-                echo $e;
+            if (Config::mode() !== 'prod') {
+                echo $e; // In development mode, display the exception
             } else {
-                $httpResponse->redirect(Router::generateUrl(Route::ERR_404), $statusCode = 404);
+                $httpResponse->redirect(Router::generateUrl(Route::ERR_404), 404);
             }
         }
     }

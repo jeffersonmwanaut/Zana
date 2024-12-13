5c302edcff16c95578fff3c319af3c985671e007
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-12-13 14:11:48 +0200

    Add new methods to ArrayHelper

--- vendor/zana/ArrayHelper.php
+++ vendor/zana/ArrayHelper.php
@@ -1,9 +1,9 @@
 <?php namespace Zana;
 
 class ArrayHelper {
-    private $array;
+    private array $array;
 
-    public function __construct($array) {
+    public function __construct(array $array) {
         $this->array = $array;
     }
 
@@ -13,10 +13,7 @@ class ArrayHelper {
      * @return mixed The first element of the array, or null if the array is empty.
      */
     public function first() {
-        if (empty($this->array)) {
-            return null;
-        }
-        return $this->array[0];
+        return empty($this->array) ? null : $this->array[0];
     }
 
     /**
@@ -25,10 +22,7 @@ class ArrayHelper {
      * @return mixed The last element of the array, or null if the array is empty.
      */
     public function last() {
-        if (empty($this->array)) {
-            return null;
-        }
-        return end($this->array);
+        return empty($this->array) ? null : end($this->array);
     }
 
     /**
@@ -38,11 +32,8 @@ class ArrayHelper {
      *
      * @return mixed The element at the specified index, or null if the index is out of range.
      */
-    public function at($index) {
-        if (!isset($this->array[$index])) {
-            return null;
-        }
-        return $this->array[$index];
+    public function at(int $index) {
+        return $this->array[$index] ?? null;
     }
 
     /**
@@ -50,7 +41,66 @@ class ArrayHelper {
      *
      * @return array The entire array.
      */
-    public function all() {
+    public function all(): array {
         return $this->array;
+    }
+
+    /**
+     * Get the count of elements in the array.
+     *
+     * @return int The number of elements in the array.
+     */
+    public function count(): int {
+        return count($this->array);
+    }
+
+    /**
+     * Clear the array.
+     *
+     * @return void
+     */
+    public function clear(): void {
+        $this->array = [];
+    }
+
+    /**
+     * Add an element to the array.
+     *
+     * @param mixed $element The element to add.
+     * @return void
+     */
+    public function add($element): void {
+        $this->array[] = $element;
+    }
+
+    /**
+     * Remove an element at a specific index.
+     *
+     * @param int $index The index of the element to remove.
+     * @return mixed The removed element, or null if the index is out of range.
+     */
+    public function remove(int $index) {
+        if (isset($this->array[$index])) {
+            $removedElement = $this->array[$index];
+            unset($this->array[$index]);
+            $this->array = array_values($this->array); // Reindex the array
+            return $removedElement;
+        }
+        return null;
+    }
+
+    /**
+     * Find an element in the array based on a callback condition.
+     *
+     * @param callable $callback The callback function to evaluate each element.
+     * @return mixed The found element, or null if not found.
+     */
+    public function find(callable $callback) {
+        foreach ($this->array as $element) {
+            if ($callback($element)) {
+                return $element;
+            }
+        }
+        return null;
     }
 }

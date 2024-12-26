3d4ccfdccbce99a20b372acf6defa3485207db29
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-12-26 13:29:29 +0200

    Add Image Validation

--- vendor/zana/Image/Image.php
+++ vendor/zana/Image/Image.php
@@ -11,19 +11,41 @@ class Image extends File
     /**
      * @var ImageDimension
      */
-    protected $imageDimension;
+    protected ImageDimension $imageDimension;
 
     public function __construct(array $file)
     {
         parent::__construct($file);
+
+        // Validate that the uploaded file is an image
+        if (!$this->isImage($file['tmp_name'])) {
+            throw new \RuntimeException('Uploaded file is not a valid image.');
+        }
+
         $dimension = getimagesize($file['tmp_name']);
+        if ($dimension === false) {
+            throw new \RuntimeException('Failed to get image dimensions.');
+        }
+
         $this->imageDimension = new ImageDimension($dimension[0], $dimension[1]);
+    }
+
+    /**
+     * Check if the uploaded file is an image.
+     * @param string $filePath
+     * @return bool
+     */
+    protected function isImage(string $filePath): bool
+    {
+        $imageTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp', 'image/bmp'];
+        $fileType = mime_content_type($filePath);
+        return in_array($fileType, $imageTypes);
     }
 
     /**
      * @return ImageDimension
      */
-    public function getImageDimension()
+    public function getImageDimension(): ImageDimension
     {
         return $this->imageDimension;
     }
@@ -32,7 +54,7 @@ class Image extends File
      * @param ImageDimension $imageDimension
      * @return Image
      */
-    public function setImageDimension($imageDimension)
+    public function setImageDimension(ImageDimension $imageDimension): self
     {
         $this->imageDimension = $imageDimension;
         return $this;

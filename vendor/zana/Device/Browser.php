3142e17690bf4f7e31952d63a2d9b52d31cc5744
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-12-26 14:59:22 +0200

    Error Handling & Default Values

--- vendor/zana/Device/Browser.php
+++ vendor/zana/Device/Browser.php
@@ -2,46 +2,54 @@
 
 class Browser extends Device
 {
-    protected $name;
-    protected $version;
-    protected $createdBy;
+    protected string $name;
+    protected string $version;
+    protected string $createdBy;
 
     public function __construct()
     {
         parent::__construct();
         $browserInfo = get_browser(null, true);
-        $this->setName($browserInfo);
-        $this->setVersion($browserInfo);
-        $this->setCreatedBy($browserInfo);
+        
+        if ($browserInfo === false) {
+            // Handle the case where browser info is not available
+            $this->name = 'unknown';
+            $this->version = 'unknown';
+            $this->createdBy = 'unknown';
+        } else {
+            $this->setName($browserInfo);
+            $this->setVersion($browserInfo);
+            $this->setCreatedBy($browserInfo);
+        }
     }
 
-    public function getName()
+    public function getName(): string
     {
         return $this->name;
     }
 
-    public function getVersion()
+    public function getVersion(): string
     {
         return $this->version;
     }
 
-    public function getCreatedBy()
+    public function getCreatedBy(): string
     {
         return $this->createdBy;
     }
 
-    protected function setName(array $browserInfo)
+    protected function setName(array $browserInfo): void
     {
-        $this->name = $browserInfo['browser'];
+        $this->name = $browserInfo['browser'] ?? 'unknown';
     }
 
-    protected function setVersion(array $browserInfo)
+    protected function setVersion(array $browserInfo): void
     {
-        $this->version = $browserInfo['version'];
+        $this->version = $browserInfo['version'] ?? 'unknown';
     }
 
-    protected function setCreatedBy(array $browserInfo)
+    protected function setCreatedBy(array $browserInfo): void
     {
-        $this->createdBy = $browserInfo['browser_maker'];
+        $this->createdBy = $browserInfo['browser_maker'] ?? 'unknown';
     }
 }

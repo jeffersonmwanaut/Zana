a92a60b181d02bb80e060b186153ffa439042b38
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2024-12-26 11:13:40 +0200

    Update UrlBuilder

--- vendor/zana/Router/UrlBuilder.php
+++ vendor/zana/Router/UrlBuilder.php
@@ -1,39 +1,73 @@
 <?php namespace Zana\Router;
 
 class UrlBuilder {
-    private $route;
-    private $routeParameters = [];
-    private $queryStringParameters = [];
-    private $anchor;
+    private string $route;
+    private array $routeParameters = [];
+    private array $queryStringParameters = [];
+    private ?string $anchor = null;
 
-    public function setRoute($route) {
+    /**
+     * Set the route for the URL.
+     *
+     * @param string $route The route to set.
+     * @return $this
+     */
+    public function setRoute(string $route): self {
         $this->route = $route;
         return $this;
     }
 
-    public function setRouteParameter($key, $value) {
+    /**
+     * Set a route parameter.
+     *
+     * @param string $key The parameter key.
+     * @param mixed $value The parameter value.
+     * @return $this
+     */
+    public function setRouteParameter(string $key, $value): self {
         $this->routeParameters[$key] = $value;
         return $this;
     }
 
-    public function setQueryStringParameter($key, $value) {
+    /**
+     * Set a query string parameter.
+     *
+     * @param string $key The parameter key.
+     * @param mixed $value The parameter value.
+     * @return $this
+     */
+    public function setQueryStringParameter(string $key, $value): self {
         $this->queryStringParameters[$key] = $value;
         return $this;
     }
 
-    public function setAnchor($anchor) {
+    /**
+     * Set the anchor for the URL.
+     *
+     * @param string $anchor The anchor to set.
+     * @return $this
+     */
+    public function setAnchor(string $anchor): self {
         $this->anchor = $anchor;
         return $this;
     }
 
-    public function build() {
+    /**
+     * Build the final URL.
+     *
+     * @return string The constructed URL.
+     */
+    public function build(): string {
         $url = Router::generateUrl($this->route, $this->routeParameters);
+        
         if (!empty($this->queryStringParameters)) {
             $url .= '?' . http_build_query($this->queryStringParameters);
         }
+        
         if (!empty($this->anchor)) {
             $url .= '#' . $this->anchor;
         }
+        
         return $url;
     }
 }
